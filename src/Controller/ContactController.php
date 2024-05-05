3898fafb61915da852b4c57c55c6a14d871e2b42
NayirGalela <galela.nayir@gmail.com>
2024-05-05 02:12:36 +0200

    mise en place du de contacttype

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form'=>$form
         ]);
     }
+
 }
