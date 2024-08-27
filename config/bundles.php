d9fac763d183b3c606b99c9eed01e1659aa2e1ea
ngalela01 <149369383+ngalela01@users.noreply.github.com>
2024-08-27 18:36:27 +0200

    "Added SymfonyCasts Reset Password Bundle to the project"

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
